187ef7a56c0f5de1f75ea7ee85e530c1d0f11e1c
thahhphuong <75551549+thahhphuong@users.noreply.github.com>
2021-06-08 21:47:28 +0700

    Delete sendMail.js

--- sendMail.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = require('express-async-handler');
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
-const express =  require('express');
-const bcrypt = require('bcrypt');
-const router = express.Router();
-
